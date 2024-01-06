9d3e284bdee01245522e472ba15416d8e8e86874
Joseph <vanhayes618@gmail.com>
2024-01-05 19:08:34 -0600

    motor controller can now decellerate please ignore all the runSpeed() functions and dont ask me about it unless you want anger.

--- Centrifuge/MotorControlCode/accelerateToSpeed/accelerateToSpeed.ino
+++ Centrifuge/MotorControlCode/accelerateToSpeed/accelerateToSpeed.ino
@@ -8,13 +8,14 @@ a desire to slow down
 // Define a stepper and the pins it will use
 AccelStepper stepper(1,7,6); //1 is driving mode, 7 is pulse, 6 is direction
 
-int targSpeed = 720; // The target speed in steps/sec (maximum seems to be around 1850 for full steps with no load)
+int targSpeed = 3000; // The target speed in steps/sec (maximum seems to be around 1850 for full steps with no load)
 float currSpeed = 0; // current speed in steps/sec
-float accel = 10; //acceleration limit in steps/sec/sec
+float accel = 500; //acceleration limit in steps/sec/sec
 
 unsigned long t_prev;
 unsigned long t_curr;
 unsigned long dt;
+unsigned long t_run = 2000; //maximum run time in milliseconds
 
 void setup()
 {  
@@ -42,21 +43,62 @@ void loop()
   while(currSpeed<targSpeed){
     //every instance increase the currSpeed proportionally to the time passed since the last instance then update the setspeed command
     t_prev = t_curr;
+    stepper.runSpeed();
     t_curr = millis();
+    stepper.runSpeed();
     dt = (t_curr-t_prev);
+    stepper.runSpeed();
 
     currSpeed = currSpeed+accel*(float)dt/1000; //to calculate the new speed do the math under type float then convert to integer later. We do this because if we calculate under type integer the while loop is too fast and the currSpeed doesnt update
-
-    //Serial.println(currSpeed);
-    //Serial.println((int)round(currSpeed));
-    stepper.setSpeed((int)round(currSpeed));
+    stepper.runSpeed();
+    stepper.setSpeed(currSpeed);
     stepper.runSpeed();
   }
 
   Serial.println("Target speed reached");
   stepper.setSpeed(targSpeed);
+  t_prev = millis();
 
-  while(true){
+  while(dt<t_run){ //while the designated spin time has not been breached
+    //check to see if button has been pressed
+    //STILL TO DO
+
+    //set speed
     stepper.runSpeed();
+
+    //calculate dt
+    dt = millis()-t_prev;
   }
+
+  Serial.println("Decelerating");
+  stepper.runSpeed();
+  
+  t_curr = millis();
+
+  while(currSpeed>0){
+    //every instance decrease the currSpeed proportionally to the time passed since the last instance then update the setspeed command
+    //Serial.println(currSpeed);
+    t_prev = t_curr;
+    stepper.runSpeed();
+    t_curr = millis();
+    stepper.runSpeed();
+    dt = (t_curr-t_prev);
+    stepper.runSpeed();
+
+    currSpeed = currSpeed-accel*(float)dt/1000;
+    stepper.runSpeed(); //to calculate the new speed do the math under type float then convert to integer later. We do this because if we calculate under type integer the while loop is too fast and the currSpeed doesnt update
+
+    stepper.setSpeed(currSpeed);
+    stepper.runSpeed();
+    
+  }
+
+  
+  stepper.setSpeed(500);
+  stepper.runSpeed();
+
+  while(true){ //pause here until reset button is pressed NOT YET IMPLEMENTED
+
+  }
+
 }
